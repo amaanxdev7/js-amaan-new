b1b8c8353688198727408a386db362680c3acb40
amaanxdev7 <amaanahmadkarkhi@gmail.com>
2026-06-26 09:28:57 +0000

    variables learn

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "amaan@gmail.com"
+var account = "amaan"
+accountName = "ahmad"
+
+// accountId = 2
+
+accountEmail = "ahmad@gmail.com"
+account = "poco"
+
+console.log(accountId)
+console.table([accountEmail,account])
